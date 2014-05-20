84d853810b9c89114c7623f2a615e89418fb585f
Michael Voelske <mvoelske@gmail.com>
2014-05-20 07:47:23 +0200

    type bounds and covariance

--- lecture/src/main/scala/week04/List.scala
+++ lecture/src/main/scala/week04/List.scala
@@ -1,11 +1,19 @@
 package week04
 
-trait List[T] {
+trait List[+T] { // "+T" -> List is covariant w.r.t. T 
+  // i.e., List of X is a subtype of List of Y iff X is a subtype of Y
   def isEmpty: Boolean
   def head: T
   def tail: List[T]
+
+  // lower bound on the type of elements passed to prepend: 
+  // only supertypes of T are allowed. This enforces the Liskov
+  // substitution principle: if we want to prepend an element that
+  // is not of a subtype of T, we must generalize the type of the
+  // resulting List (the compiler does this automatically)
+  def prepend[U >: T](elem: U): List[U] = new Cons(elem, this)
   
-  override def toString : String = 
+  override def toString: String =
     if (isEmpty) "()"
     else "(" + head.toString + ", " + tail.toString + ")"
 }
@@ -15,14 +23,14 @@ class Cons[T](val head: T, val tail: List[T]) extends List[T] {
   def isEmpty = false
 }
 
-class Nil[T] extends List[T] {
+object Nil extends List[Nothing] {
   def isEmpty = true
   def head: Nothing = throw new NoSuchElementException("Nil.head")
   def tail: Nothing = throw new NoSuchElementException("Nil.tail")
 }
 
 object List {
-  def apply[T]() = new Nil[T]
-  def apply[T](x: T) = new Cons[T](x, new Nil[T])
-  def apply[T](x: T, y: T) = new Cons[T](x, new Cons[T](y, new Nil[T]))
+  def apply[T]() = Nil
+  def apply[T](x: T) = new Cons[T](x, Nil)
+  def apply[T](x: T, y: T) = new Cons[T](x, new Cons[T](y, Nil))
 }
--- lecture/src/main/scala/week04/bounds.sc
+++ lecture/src/main/scala/week04/bounds.sc
@@ -5,8 +5,15 @@ object bounds {
   
   def foo[S >: NonEmpty <: IntSet](xs: S) = ???   //> foo: [S >: week04.NonEmpty <: week04.IntSet](xs: S)Nothing
   
-  val a : Array[NonEmpty] = Array(new NonEmpty()) //> a  : Array[week04.NonEmpty] = Array(week04.NonEmpty@299bc338)
+  val a : Array[NonEmpty] = Array(new NonEmpty()) //> a  : Array[week04.NonEmpty] = Array(week04.NonEmpty@625a80df)
 	//val b : Array[IntSet] = a // error! Arrays are invariant
+	
+	
+	// due to the type bounds on prepend, f automatically gets
+	// the correct type, List[IntSet]
+	def f(xs: List[NonEmpty], x: Empty) = xs prepend x
+                                                  //> f: (xs: week04.List[week04.NonEmpty], x: week04.Empty)week04.List[week04.Int
+                                                  //| Set]
 }
 
 trait IntSet
